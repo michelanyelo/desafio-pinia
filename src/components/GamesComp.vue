2a753b2e5d293ac9df75d00e78ffcc43b8caa0e5
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-09-12 19:07:18 -0300

    M7D6 desafio: Refactor CSS and HTML structure in base.css, main.css, and GamesComp.vue

--- src/components/GamesComp.vue
+++ src/components/GamesComp.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
+    <div class="table-responsive mt-5 text-center">
+        <table class="table w-75 mx-auto">
+            <thead class="text-uppercase table-light">
                 <tr>
-                    <th scope="col" v-for="(head, index) in headers" :key="index">{{ head }}
+                    <th scope="col" v-for="(head, index) in headers" :key="index">{{
+                        head }}
                     </th>
+                    <th>Acción</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="game in gamesStore.games" :key="game.codigo">
                     <td v-for="(value, index) in Object.values(game).slice(0, -1)" :key="index">{{ value }}
+                    </td>
+                    <td>
+                        <button class="btn btn-outline-success btn-sm me-3">Agregar</button>
+                        <button class="btn btn-outline-danger btn-sm">Quitar</button>
                     </td>
                 </tr>
             </tbody>
