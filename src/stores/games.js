b3a6e4955b30e445bb73fc63f20a5469a75b99fd
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-09-12 19:50:37 -0300

    M7D6 desafio: Refactor games store: added whitespace, removed unused variables, and reformatted code

--- src/stores/games.js
+++ src/stores/games.js
@@ -1,36 +1,39 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-import axios from "axios";
+import { defineStore } from "pinia"
+import { ref } from "vue"
+import axios from "axios"
 
 export const useGamesStore = defineStore("games", () => {
+    // Estado reactivo para almacenar los juegos
     const games = ref(null)
-    // const aumentar = ref(0)
-    // const disminuir = ref(0)
+
+    // Función para obtener la lista de juegos desde la API
     async function fetchGames() {
         try {
             const url = '/api/juegos.json'
             const response = await axios.get(url)
             const { data } = response
-            games.value = data
+            games.value = data // Actualiza el estado con los datos obtenidos
         } catch (error) {
-            console.error(error)
+            console.error(error) // Manejo de errores en caso de fallo en la petición
         }
     }
 
+    // Función para aumentar el stock de un juego específico
     function aumentarStock(id) {
         const game = games.value.find((game) => game.codigo === id)
         if (game) {
-            game.stock++
+            game.stock++ // Incrementa el stock del juego
         }
     }
 
+    // Función para disminuir el stock de un juego específico
     function disminuirStock(id) {
         const game = games.value.find((game) => game.codigo === id)
         game ?
             game.stock > 0 ?
-                game.stock-- :
-                alert(`No hay suficiente stock para rebajar del juego: ${game.nombre}`)
-            : alert('Juego no encontrado');
+                game.stock-- : // Decrementa el stock si hay suficiente
+                alert(`No hay suficiente stock para rebajar del juego: ${game.nombre}`) // Mensaje si no hay suficiente stock
+            : alert('Juego no encontrado') // Mensaje si el juego no es encontrado
     }
 
     return {
@@ -38,5 +41,5 @@ export const useGamesStore = defineStore("games", () => {
         fetchGames,
         aumentarStock,
         disminuirStock
-    }
-})
+    };
+});
